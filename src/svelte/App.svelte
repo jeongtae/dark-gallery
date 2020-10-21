a2e151cd4c287c8d044c5b5f35bc0707997f8c19
Jeongtae Kim <rsatang5@naver.com>
2020-10-21 22:57:29 +0900

    기본 템플릿 삭제

--- src/svelte/App.svelte
+++ src/svelte/App.svelte
@@ -1,9 +1,6 @@
 <script lang="ts">
   const { Titlebar, Color } = require("custom-electron-titlebar");
   import { onMount } from "svelte";
-  import Link from "./Link.svelte";
-
-  export let name: string;
 
   onMount(() => {
     new Titlebar({
@@ -16,30 +13,8 @@
   <title>Hello</title>
 </svelte:head>
 <template>
-  <main>
-    <h1>Hello {name}!</h1>
-    <p>
-      Visit the
-      <Link href="https://svelte.dev/tutorial">Svelte tutorial</Link>
-      to learn how to build Svelte apps.
-    </p>
-  </main>
+  <div />
 </template>
 
 <style lang="scss">
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-    @media (min-width: 640px) {
-      max-width: none;
-    }
-  }
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
 </style>
