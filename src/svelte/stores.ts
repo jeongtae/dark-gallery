4f0815db9f4a409140fbfe32a86eae02c59ca9db
Jeongtae Kim <rsatang5@naver.com>
2020-10-28 22:11:29 +0900

    stores.ts에 스토리지 스토어 추가

--- src/svelte/stores.ts
+++ src/svelte/stores.ts
@@ -1,3 +1,29 @@
 import { writable } from "svelte/store";
+import { storageWritable } from "./storage";
 
 export const galleryPath = writable<string>("");
+
+interface RecentGalleryInfo {
+  title: string;
+  path: string;
+}
+export const recentGalleryInfoListStore = storageWritable<RecentGalleryInfo[]>(
+  "recent-gallery-info-list",
+  []
+);
+
+enum ColorTheme {
+  Dark,
+  Light,
+  Auto,
+}
+interface GlobalSettings {
+  colorTheme?: ColorTheme;
+  maxRecentGalleriesCount?: number;
+}
+export const defaultGlobalSettings: GlobalSettings = {
+  colorTheme: ColorTheme.Dark,
+  maxRecentGalleriesCount: 5,
+};
+export const globalSettingsStore = storageWritable<GlobalSettings>("global-settings", {});
+globalSettingsStore.update(settings => ({ ...defaultGlobalSettings, ...settings }));
