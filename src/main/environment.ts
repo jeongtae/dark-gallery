330761f6ef96b7181a3bf17d04ecc47357bd491b
Jeongtae Kim <rsatang5@naver.com>
2020-11-16 09:21:33 +0900

    electron-is 패키지로 electron-is-dev 패키지를 대치

--- src/main/environment.ts
+++ src/main/environment.ts
@@ -1,8 +1,9 @@
 import { app } from "electron";
+import is from "electron-is";
 
 const isSquirrelStartup: boolean = require("electron-squirrel-startup");
-const isDev: boolean = require("electron-is-dev");
-const isMac: boolean = process.platform === "darwin";
+const isDev: boolean = is.dev();
+const isMac: boolean = is.macOS();
 const appName: string = app.name;
 
 export { isSquirrelStartup, isDev, isMac, appName };
--- src/svelte/global.d.ts
+++ src/svelte/global.d.ts
@@ -6,5 +6,5 @@ interface NodeRequire {
   (id: "electron/main"): typeof Electron.Main;
   (id: "electron/renderer"): typeof Electron.Renderer;
   (id: "custom-electron-titlebar"): typeof import("custom-electron-titlebar");
-  (id: "electron-is-dev"): typeof import("electron-is-dev");
+  (id: "electron-is"): typeof import("electron-is");
 }
