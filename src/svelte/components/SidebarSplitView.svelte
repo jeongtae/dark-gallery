c2b9e7d6c0c0c1d5a01c8d29632d39680b17a977
Jeongtae Kim <rsatang5@naver.com>
2020-12-09 23:22:53 +0900

    SidebarSplitView 개선

--- src/svelte/components/SidebarSplitView.svelte
+++ src/svelte/components/SidebarSplitView.svelte
@@ -5,22 +5,18 @@
   export let sidebarWidth = 200;
   export let sidebarMinWidth = 100;
   export let sidebarMaxWidth = 300;
-  let containerRef, sidebarRef;
+  export let throttling = 20;
 
+  let containerRef;
   let draggable = false;
   let dragging = false;
+  let mounted = false;
+  onMount(() => (mounted = true));
 
-  $: if (containerRef) {
-    containerRef.style.cursor = draggable || dragging ? "col-resize" : "auto";
-  }
-  $: if (sidebarRef) {
-    sidebarRef.style.width = `${sidebarWidth}px`;
-  }
-  onMount(() => {
-    sidebarRef.style.width = `${sidebarWidth}px`;
-  });
+  $: containerCursor = draggable || dragging ? "col-resize" : "auto";
 
-  function handleMouseMove({ clientX }) {
+  const handleMouseMove = throttle(({ clientX }) => {
+    if (!mounted) return;
     const { left } = containerRef.getBoundingClientRect();
     const localX = clientX - left;
     const diff = Math.abs(localX - sidebarWidth);
@@ -32,8 +28,7 @@
       newWidth = Math.round(newWidth);
       sidebarWidth = newWidth;
     }
-  }
-  const throttledHandleMouseMove = throttle(handleMouseMove, 20);
+  }, throttling);
 
   function handleMouseDown(e) {
     if (draggable) {
@@ -53,12 +48,13 @@
 <div
   class="container"
   bind:this={containerRef}
-  on:mousemove={throttledHandleMouseMove}
+  on:mousemove={handleMouseMove}
   on:mousedown={handleMouseDown}
   on:mouseup={handleMouseUp}
   on:mouseleave={handleMouseUp}
+  style="cursor: {containerCursor};"
 >
-  <div class="sidebar" bind:this={sidebarRef}>
+  <div class="sidebar" style="width: {sidebarWidth}px;">
     <slot name="sidebar" />
   </div>
   <div class="main">
