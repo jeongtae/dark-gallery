2d0830409a4ae10d574778f30f925b28f3b08084
Jeongtae Kim <rsatang5@naver.com>
2020-12-19 20:55:16 +0900

    GalleryPage 디자인 일부 완성

--- src/svelte/pages/GalleryPage.svelte
+++ src/svelte/pages/GalleryPage.svelte
@@ -1,55 +1,416 @@
 <script lang="ts">
   const path = require("path");
   import type { RawItem } from "../../common/sequelize";
+  import oc from "open-color";
+  import { onMount } from "svelte";
+  import { fade, fly } from "svelte/transition";
+  import { Icon, Button } from "carbon-components-svelte";
+  import Filter24 from "carbon-icons-svelte/lib/Filter24";
+  import InformationSquare24 from "carbon-icons-svelte/lib/InformationSquare24";
+  import CaretLeft16 from "carbon-icons-svelte/lib/CaretLeft16";
+  import CaretRight16 from "carbon-icons-svelte/lib/CaretRight16";
+  import PageLast24 from "carbon-icons-svelte/lib/PageLast24";
+  import PageFirst24 from "carbon-icons-svelte/lib/PageFirst24";
+  import ZoomIn24 from "carbon-icons-svelte/lib/ZoomIn24";
+  import ZoomOut24 from "carbon-icons-svelte/lib/ZoomOut24";
+  import Minimize16 from "carbon-icons-svelte/lib/Minimize16";
+  import Maximize16 from "carbon-icons-svelte/lib/Maximize16";
+  import TrashCan24 from "carbon-icons-svelte/lib/TrashCan24";
+  import Favorite24 from "carbon-icons-svelte/lib/Favorite24";
+  import Close24 from "carbon-icons-svelte/lib/Close24";
   import ipc from "../ipc";
   import { currentGalleryInfoStore } from "../stores";
-  import SidebarSplitView from "../components/SidebarSplitView.svelte";
   import VirtualGrid from "../components/VirtualGrid.svelte";
   import GridItem from "../components/GridItem.svelte";
 
+  onMount(async () => {
+    items = await ipc.invoke("getItems");
+    console.dir(items);
+  });
+
+  let showFilterSidebar = false;
+  let showDetailSidebar = false;
+
   let items: RawItem[] = [];
   let selectedItemKeys: Set<string> = new Set();
+
+  let gridItemFitCoverMode = true;
+  let gridItemsPerRow = 7;
+  let gridItemsPerRowMin = 5;
+  let gridItemsPerRowMax = 8;
+
+  const controlButtonClickHandlers = {
+    openFilterSidebar() {
+      showFilterSidebar = true;
+    },
+    openDetailSidebar() {
+      showDetailSidebar = true;
+    },
+    zoomIn() {
+      if (gridItemsPerRow > gridItemsPerRowMin) {
+        gridItemsPerRow -= 1;
+      }
+    },
+    zoomOut() {
+      if (gridItemsPerRow < gridItemsPerRowMax) {
+        gridItemsPerRow += 1;
+      }
+    },
+    toggleFitCover() {
+      gridItemFitCoverMode = !gridItemFitCoverMode;
+    },
+    toggleSelectedItemsFavorite() {
+      console.error("NOT IMPLEMENTED");
+    },
+    deleteSelectedItems() {
+      console.error("NOT IMPLEMENTED");
+    },
+    deselectAllItem() {
+      selectedItemKeys.clear();
+      selectedItemKeys = selectedItemKeys;
+    },
+  };
+  const filterSidebarButtonClickHandlers = {
+    close() {
+      showFilterSidebar = false;
+    },
+  };
+  const detailSidebarButtonClickHandlers = {
+    close() {
+      showDetailSidebar = false;
+    },
+  };
+  function startIndexing() {
+    ipc.invoke("startIndexing");
+  }
+
+  async function getItems() {
+    items = await ipc.invoke("getItems");
+    console.dir(items);
+  }
 </script>
 
-<SidebarSplitView>
-  <div slot="sidebar">
-    <button
-      on:click={() => {
-        ipc.invoke('startIndexing');
-      }}
-    >Start Index</button>
-    <button
-      on:click={async () => {
-        items = await ipc.invoke('getItems');
-        console.dir(items);
-      }}
-    >Get Items</button>
-  </div>
-  <div class="main">
-    <VirtualGrid {items} itemKeyProp="id" gap={2} let:item>
-      <GridItem
-        on:click={() => {
-          if (selectedItemKeys.has(item.id)) {
-            selectedItemKeys.delete(item.id);
-            selectedItemKeys = selectedItemKeys;
-          } else {
-            selectedItemKeys = selectedItemKeys;
-            selectedItemKeys.add(item.id);
-          }
-        }}
-        selected={selectedItemKeys.has(item.id)}
-        thumbnailBase64={item.thumbnailBase64}
-        thumbnailPath={path.join($currentGalleryInfoStore.path, item.thumbnailPath)}
-      />
-    </VirtualGrid>
-  </div>
-</SidebarSplitView>
+<page-container>
+  <page-split-view>
+    <!-- FILTER SIDEBAR-->
+    {#if showFilterSidebar}
+      <page-split-side style="width: 250px" in:fly={{ x: -250 }}>
+        <page-filter-sidebar>
+          <page-sidebar-header>
+            <page-flex style="align-items: center;">
+              <Icon render={Filter24} />
+              <span style="margin: 0 4px; font-weight: 500; font-size: 1.4rem;">필터</span>
+            </page-flex>
+            <button class="close" on:click={filterSidebarButtonClickHandlers.close}>
+              <Icon render={PageFirst24} />
+            </button>
+          </page-sidebar-header>
+          CONTENTS AREA
+        </page-filter-sidebar>
+      </page-split-side>
+    {/if}
+    <page-split-main>
+      <!-- MAIN - CONTROL BAR -->
+      <page-control-bar class="top" in:fade={{ delay: 500, duration: 200 }}>
+        <page-flex style="justify-content: space-between; padding: 8px 0;">
+          <!-- CONTROL BAR - LEFT SIDE -->
+          <page-flex>
+            {#if !showFilterSidebar}
+              <page-control-strip class="left-collapsed">
+                <button
+                  style="width: auto; padding-left: 5px;"
+                  on:click={controlButtonClickHandlers.openFilterSidebar}
+                >
+                  <Icon render={Filter24} />
+                  <Icon render={CaretRight16} />
+                </button>
+              </page-control-strip>
+            {/if}
+            <page-control-strip style="margin-left: 12px">
+              <button on:click={controlButtonClickHandlers.zoomOut}>
+                <Icon render={ZoomOut24} />
+              </button>
+              <page-control-strip-split />
+              <button on:click={controlButtonClickHandlers.zoomIn}>
+                <Icon render={ZoomIn24} />
+              </button>
+            </page-control-strip>
+            <page-control-strip style="margin-left: 12px; position: relative;">
+              <button on:click={controlButtonClickHandlers.toggleFitCover}>
+                <div
+                  style="
+                    position: absolute;
+                    box-shadow: 0 0 0 1.5px {oc.white};
+                    border-radius: 1.5px;
+                    width: {57}%; height: {57}%;"
+                />
+                <Icon render={gridItemFitCoverMode ? Minimize16 : Maximize16} />
+              </button>
+            </page-control-strip>
+          </page-flex>
+          <!-- CONTROL BAR - RIGHT SIDE -->
+          <page-flex style="flex: 1; justify-content: flex-end;">
+            <div style="position: relative; flex: 1;">
+              {#if selectedItemKeys && selectedItemKeys.size > 0}
+                <page-control-strip
+                  style="position: absolute; right: 12px;"
+                  in:fade={{ delay: 200, duration: 200 }}
+                  out:fade={{ duration: 200 }}
+                >
+                  <span>{selectedItemKeys.size}개 선택됨</span>
+                  <page-control-strip-split />
+                  <button on:click={controlButtonClickHandlers.toggleSelectedItemsFavorite}>
+                    <div class="not-hover">
+                      <Icon render={Favorite24} />
+                    </div>
+                    <div class="hover" style="color: {oc.pink[6]};">
+                      <Icon render={Favorite24} />
+                    </div>
+                  </button>
+                  <page-control-strip-split />
+                  <button on:click={controlButtonClickHandlers.deleteSelectedItems}>
+                    <div class="not-hover">
+                      <Icon render={TrashCan24} />
+                    </div>
+                    <div class="hover" style="color: {oc.red[7]};">
+                      <Icon render={TrashCan24} />
+                    </div>
+                  </button>
+                  <page-control-strip-split />
+                  <button on:click={controlButtonClickHandlers.deselectAllItem}>
+                    <Icon render={Close24} />
+                  </button>
+                </page-control-strip>
+              {:else}
+                <page-control-strip
+                  class="transparent"
+                  style="position: absolute; right: 12px; pointer-events: none;"
+                  in:fade={{ delay: 200, duration: 200 }}
+                  out:fade={{ duration: 200 }}
+                >
+                  <span>{items?.length ?? 0}개의 항목</span>
+                </page-control-strip>
+              {/if}
+            </div>
+            {#if !showDetailSidebar}
+              <page-control-strip class="right-collapsed">
+                <button
+                  style="width: auto; padding-right: 5px;"
+                  on:click={controlButtonClickHandlers.openDetailSidebar}
+                >
+                  <Icon render={CaretLeft16} />
+                  <Icon render={InformationSquare24} />
+                </button>
+              </page-control-strip>
+            {/if}
+          </page-flex>
+        </page-flex>
+      </page-control-bar>
+      <!-- MAIN - GALLERY GRID -->
+      <div style="height: 100%">
+        <VirtualGrid
+          {items}
+          itemKeyProp="id"
+          gap={2}
+          bind:itemsPerRow={gridItemsPerRow}
+          minItemsPerRow={gridItemsPerRowMin}
+          maxItemsPerRow={gridItemsPerRowMax}
+          paddingSide={2}
+          paddingTop={50}
+          paddingBottom={2}
+          let:item
+        >
+          <GridItem
+            on:click={() => {
+              if (selectedItemKeys.has(item.id)) {
+                selectedItemKeys.delete(item.id);
+                selectedItemKeys = selectedItemKeys;
+              } else {
+                selectedItemKeys = selectedItemKeys;
+                selectedItemKeys.add(item.id);
+              }
+            }}
+            selected={selectedItemKeys.has(item.id)}
+            selectedItemBorderMode={gridItemsPerRow > 5 ? 'thin' : 'normal'}
+            thumbnailBase64={item.thumbnailBase64}
+            thumbnailPath={path.join($currentGalleryInfoStore.path, item.thumbnailPath)}
+            thumbnailAspect={item.width / item.height}
+            thumbnailFitMode={gridItemFitCoverMode ? 'cover' : 'contain'}
+          />
+        </VirtualGrid>
+      </div>
+    </page-split-main>
+    <!-- DETAIL SIDEBAR-->
+    {#if showDetailSidebar}
+      <page-split-side class="dark" style="width: 250px" in:fly={{ x: 250 }}>
+        <page-detail-sidebar>
+          <page-sidebar-header>
+            <button class="close" on:click={detailSidebarButtonClickHandlers.close}>
+              <Icon render={PageLast24} />
+            </button>
+          </page-sidebar-header>
+          CONTENTS AREA
+        </page-detail-sidebar>
+      </page-split-side>
+    {/if}
+  </page-split-view>
+  <page-status-bar>STATUS BAR AREA</page-status-bar>
+</page-container>
 
 <style lang="scss">
   @import "open-color/open-color";
 
-  .main {
+  $grid-back-color: mix($oc-gray-9, $oc-black, 70%);
+  $control-bar-height: 50px;
+  $transition-duration: 120ms;
+  $button-back-color: rgba($grid-back-color, 50%);
+  $button-back-color-hover: rgba(scale-color($grid-back-color, $lightness: 50%), 25%);
+
+  page-container {
+    display: flex;
+    flex-direction: column;
     height: 100%;
-    background: mix($oc-gray-9, $oc-black, 70%);
+  }
+
+  page-split-view {
+    flex: 1;
+    overflow: hidden;
+    display: flex;
+    background-color: $grid-back-color;
+    page-split-side {
+      background-color: $oc-gray-9;
+      display: block;
+      &.dark {
+        background-color: inherit;
+      }
+    }
+    page-split-main {
+      flex: 1;
+      display: block;
+      position: relative;
+    }
+  }
+
+  page-control-bar {
+    position: absolute;
+    left: 0;
+    right: 0;
+    height: 100px;
+    z-index: 5;
+    background-blend-mode: darken;
+    pointer-events: none;
+    &.top {
+      display: block;
+      width: 100%;
+      height: 100px;
+      background: linear-gradient(rgba($grid-back-color, 50%), transparent 50%);
+    }
+    // &.bottom {
+    //   display: flex;
+    //   align-items: flex-end;
+    //   bottom: 0;
+    //   background: linear-gradient(transparent 50%, rgba($grid-back-color, 50%));
+    // }
+  }
+
+  page-control-strip {
+    display: flex;
+    border-radius: 5px;
+    backdrop-filter: blur(4px);
+    pointer-events: auto;
+    user-select: none;
+    overflow: hidden;
+    &.left-collapsed {
+      border-radius: 0 5px 5px 0;
+    }
+    &.right-collapsed {
+      border-radius: 5px 0 0 5px;
+    }
+    > * {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 34px;
+      height: 34px;
+      color: $oc-gray-0;
+      background-color: $button-back-color;
+      transition: background-color $transition-duration ease-in-out;
+    }
+    > button {
+      position: relative;
+      .not-hover,
+      .hover {
+        position: absolute;
+        transition: opacity $transition-duration ease-in-out;
+      }
+      .hover {
+        opacity: 0;
+      }
+      &:hover {
+        background-color: $button-back-color-hover;
+        .not-hover {
+          opacity: 0;
+        }
+        .hover {
+          opacity: 1;
+        }
+      }
+    }
+    > span {
+      width: unset;
+      padding: 0 12px;
+      font-weight: 600;
+    }
+    > page-control-strip-split {
+      width: 1px;
+      background-color: scale-color($button-back-color, $alpha: -50%);
+    }
+    &.transparent {
+      backdrop-filter: unset;
+      > * {
+        background-color: unset;
+      }
+      // > button:hover {
+      //   background-color: unset;
+      // }
+      > span {
+        text-shadow: 0 0 6px $oc-black;
+      }
+    }
+  }
+
+  page-sidebar-header {
+    height: $control-bar-height;
+    display: flex;
+    align-items: center;
+    padding: 0 12px;
+    justify-content: space-between;
+    button.close {
+      width: 34px;
+      height: 34px;
+      color: $oc-white;
+      border-radius: 5px;
+      transition: background-color $transition-duration ease-in-out;
+      &:hover {
+        background-color: $button-back-color-hover;
+      }
+    }
+  }
+  page-filter-sidebar {
+  }
+
+  page-detail-sidebar {
+  }
+
+  page-status-bar {
+    display: flex;
+    align-items: center;
+    height: 24px;
+    padding: 0 12px;
+    background-color: $oc-gray-8;
+  }
+
+  page-flex {
+    display: flex;
   }
 </style>
