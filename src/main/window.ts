4d856829e7002c8867c8376ceccea2a2f1331191
Jeongtae Kim <rsatang5@naver.com>
2020-12-29 01:02:09 +0900

    webContents의 contextIsolation 값 지정

--- src/main/window.ts
+++ src/main/window.ts
@@ -16,6 +16,7 @@ export function createWindow(): BrowserWindow {
       devTools: isDev,
       nodeIntegration: true,
       enableRemoteModule: true,
+      contextIsolation: false,
     },
   });
 
